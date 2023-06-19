ac24e3b501fad8d976198af4682b16cde2d86b0d
RAHUL-HACKER-HACKER <rahulk841501@gmail.com>
2023-06-19 12:35:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import UserTable from './UserTable';
+
+function App() {
+  return (
+    <div className="App">
+      <UserTable></UserTable>
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UserTable.js
@@ -0,0 +1,36 @@
+
+import React, { useState, useEffect } from 'react';
+
+function UserTable() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dummyison.com/users')
+      .then(response => response.json())
+      .then(data => setUsers(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map(user => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default UserTable;
